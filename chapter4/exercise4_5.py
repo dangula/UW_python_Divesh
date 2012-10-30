cbb1803cb7d9199f74d0271325a9a5f06c862db6
dangula <divesh.an@live.com>
2012-10-29 19:17:32 -0700

    chapter 4

--- chapter4/exercise4_5.py
+++ chapter4/exercise4_5.py
@@ -2,17 +2,21 @@ from swampy.TurtleWorld import *
 import math
 
 def spiral(t,n,length,angle):
+    theta = 0.0
+    a=2
+    b=3
     for i in range(n):
         fd(t,length)
+        dtheta = 1 / (a + b * theta)
         lt(t,angle)
-        angle = angle+(.5)
+        theta += dtheta
         
 
 world = TurtleWorld()
 bob = Turtle()
 
-bob.delay = 0.01
+bob.delay = 0.001
 
-spiral(bob, 70, 10,5)
+spiral(bob, 1000, 3,5)
 
 wait_for_user()
